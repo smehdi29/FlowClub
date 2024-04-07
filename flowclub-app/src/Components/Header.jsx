0bc433c8f6f68808f07fada57495654e00c214bf
Syed Shaban <syed.shaban.21@gmail.com>
2024-04-07 18:31:48 -0500

    Added basic layout/template/outline for things to do, and realized I was working on master branch

---
+++ flowclub-app/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './css/Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Flow.Club</h1>
+        </header>
+    );
+}
+
+export default Header;
--- flowclub-app/src/Components/LandingPage.jsx
+++ flowclub-app/src/Components/LandingPage.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/LandingPage.css';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 
 const LandingPage = () => {
     const navigate = useNavigate();
 
     return (
       <div className="landingPageContainer">
+        <Header />
+        <div className="landingPageContent">
+          <h2>Flow.Club</h2>
+          <p>Fight procrastination.<br />Get your work done.<br />Go have fun.</p>
+          <button onClick={() => navigate('/login')}>Login</button>
+        </div>
+        <div className="landingPageImage">
+          <img src="path_to_image" alt="Host" />
+        </div>
+        <Footer />
       </div>
     )
 }
