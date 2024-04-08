c03ce17f90f3eb2ce885da75acf17095f6dd15af
Syed Shaban <syed.shaban.21@gmail.com>
2024-04-07 20:14:57 -0500

    added routes to App.js for pages

--- flowclub-app/src/App.js
+++ flowclub-app/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
+import Homepage from './Components/Homepage';
+import LoginPage from './Components/LoginPage';
+import CreateAccount from './Components/CreateAccount';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<LandingPage />} />
+        <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/signup" exact element={<CreateAccount />} />
+        <Route path="/home" exact element={<Homepage />} />
       </Routes>
     </Router>
   );
