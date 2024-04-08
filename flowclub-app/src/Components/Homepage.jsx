4da1ad72bb1b98158909b8646b159d212d4b113a
Oscar Franco <42709380+FrancoOscar@users.noreply.github.com>
2024-04-08 15:57:05 -0500

    Added video call

--- flowclub-app/src/Components/Homepage.jsx
+++ flowclub-app/src/Components/Homepage.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/Homepage.css';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
+import { VideoRoom } from './VideoRoom';
 
 const Homepage = () => {
     const navigate = useNavigate();
+    const [joined, setJoined] = useState(false);
 
     return (
       <div className="homepageContainer">
         <Header />
+        {!joined && (
+          <button onClick={() => setJoined(true)}>
+            Join Room
+          </button>
+        )}
+        {joined && <VideoRoom />}
         <Footer />
       </div>
-    )
+    );
 }
+
 export default Homepage;
