186f570c749d0417cc62f1561f4608c89ad45fdf
Syed Shaban <syed.shaban.21@gmail.com>
2024-04-14 21:44:18 -0500

    Added Calendar to homepage

--- flowclub-app/src/Components/Homepage.jsx
+++ flowclub-app/src/Components/Homepage.jsx
@@ -4,10 +4,21 @@ import './css/Homepage.css';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import NavBar from './NavBar.jsx';
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
 
 const Homepage = () => {
     const navigate = useNavigate();
     const navigateTo = (path) => () => navigate(path);
+    const events = [
+      { start: "2024-04-14T09:00:00", end: "2024-04-14T11:00:00", id: "1", text: "Event 1" },
+  ];
+
+  const calendarConfig = {
+      viewType: "Week",
+      startDate: DayPilot.Date.today(),
+      events: events
+  };
+
     return (
       <div className="homepageContainer">
         <Header />
@@ -21,7 +32,9 @@ const Homepage = () => {
           <NavBar />
           <div className="calendarSessionsContainer">
             <div className="calendarContainer">
-
+              <DayPilotCalendar
+                {...calendarConfig}
+              />
             </div>
             <div className="sessionButtonsContainer">
             <button onClick={navigateTo('/scheduleSession')} className="scheduleSessionButtonHP">
