11fab02ef27511a6e34e0be02d6667a6f94fc477
smehdi29 <syed.business29@gmail.com>
2024-04-23 14:49:45 -0500

    added header to join session

--- flowclub-app/src/Components/JoinSession.jsx
+++ flowclub-app/src/Components/JoinSession.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/JoinSession.css';
+import Header from './Header';
 
 function JoinSession() {
   const navigate = useNavigate();
@@ -14,17 +15,21 @@ function JoinSession() {
   };
 
   return (
-    <div className="JoinSession">
-      <div className="centered">
-        <h1>Join a Meeting!</h1>
-        <h2>Enter the meeting link found in your email</h2>
-        <input type="text" className="inputBox" placeholder="Meeting Link" />
-        <div className="buttonsContainer">
-          <button onClick={handleJoinUser}>Join</button>
-          <button onClick={handleJoinHost}>Join as a host</button>
+    <div className="joinSessionContainer">
+      <Header />
+      <div className="JoinSession">
+        <div className="centered">
+          <h1>Join a Meeting!</h1>
+          <h2>Enter the meeting link found in your email</h2>
+          <input type="text" className="inputBox" placeholder="Meeting Link" />
+          <div className="buttonsContainer">
+            <button onClick={handleJoinUser}>Join</button>
+            <button onClick={handleJoinHost}>Join as a host</button>
+          </div>
         </div>
       </div>
     </div>
+    
   );
 }
 
